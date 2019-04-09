f0b466cdda9419cc3c73d145d15f9d5df2ca5af2
jecomas <javier@leapfactor.com>
2019-04-09 16:58:56 -0300

    fixes name

--- test/credit_card_formatter_test.dart
+++ test/credit_card_formatter_test.dart
@@ -35,7 +35,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: CreditCard(
           cardWidth: 300.0,
           tradeMark: TradeMarkWidget.MASTERCARD,
-          fullName: "Jhon E. Doe",
+          fullName: "John E. Doe",
           pan: "5543  2234  2344  2234",
           expiration: "02/20",
         ),
